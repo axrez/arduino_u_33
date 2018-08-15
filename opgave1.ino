5cf912f8dc57c18cfd28b6b0f0623468e4dcfcdd
Emil <isdragon112@gmail.com>
2018-08-15 12:15:35 +0200

    whatever

---
+++ opgave1.ino
@@ -0,0 +1,19 @@
+int knap1pin = 4;
+int knap2pin = 5;
+int ledPin = 3;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(knap1pin, INPUT);
+  pinMode(knap2pin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(digitalRead(knap1pin)) {
+    digitalWrite(ledPin, HIGH);
+  }
+}
